2f011ca9110d808a2b43e40489d882768c53dde2
Duc <duc.nvd.2002@gmail.com>
2023-05-27 22:34:35 +0700

    update

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
-        self.asserEqual('foo'.upper(), "FOO")
+        self.assertEqual('foo'.upper(), "FOO")
 
     def test_isupper(self):
-        self.asserTrue('FOO'.isupper())
+        self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
